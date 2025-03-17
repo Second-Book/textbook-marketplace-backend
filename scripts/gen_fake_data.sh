d1735b150c81f13a2aaa43adcd42a4cc73bec095
Sasha <Geimer0073@yandex.ru>
2025-03-17 23:43:50 +0500

    add docs and echos for execution

--- scripts/gen_fake_data.sh
+++ scripts/gen_fake_data.sh
@@ -1,4 +1,9 @@
-#!bin/bash
+#!/bin/bash
+
+: '
+Usage example:
+bash gen_fake_data.sh 5
+'
 
 if [ -z "$1" ]; then
   echo "Error, provide amount of users and textbooks to generate."
@@ -7,5 +12,8 @@ fi
 
 count="$1"
 
+echo "Generating fake users..."
 uv run python ../textbook_marketplace/manage.py generate_fake_users $count
+
+echo "Generating fake textbooks..."
 uv run python ../textbook_marketplace/manage.py generate_fake_textbooks $count
