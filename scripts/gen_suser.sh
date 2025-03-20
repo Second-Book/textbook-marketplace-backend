fd120d6cb1949fd3ad30de2dba33060f4f31deb0
Sasha <Geimer0073@yandex.ru>
2025-03-21 04:27:54 +0500

    rm envs usage; user hardcoded vals instead

--- scripts/gen_suser.sh
+++ scripts/gen_suser.sh
@@ -4,20 +4,11 @@
 !Git Bash required!
 This script helps create superuser to get access to django admin panel.
 Credentials are taken from .env file:
-username - $DB_USER
-email - $DB_USER@example.com
+username - root
+email - root@example.com
 
 You only need to input password and repeat it to create superuser.
 '
 SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 
-env_path="$SCRIPT_DIR/../.env"
-
-if [ -f "$env_path" ]; then
-  source "$env_path"
-else
-  echo "No .env file in project root."
-  exit 1
-fi
-
-uv run winpty python "$SCRIPT_DIR/../textbook_marketplace/manage.py" createsuperuser --username "$DB_USER" --email "$DB_USER@example.com"
+uv run winpty python "$SCRIPT_DIR/../textbook_marketplace/manage.py" createsuperuser --username "root" --email "root@example.com"
