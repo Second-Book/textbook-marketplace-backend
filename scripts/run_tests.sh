c9dcef4bb6dfb958aededce9d6188618f107c360
Sasha <Geimer0073@yandex.ru>
2025-04-13 03:47:39 +0500

    update test script

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -4,13 +4,16 @@
 This script runs django backend tests and allows to see the results for 60
 seconds (can be changed).
 '
+export PYTHONDONTWRITEBYTECODE=1
 
 SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 
-echo "Running basic django tests..."
-uv run python "$SCRIPT_DIR/../textbook_marketplace/manage.py" test
+#echo "Running basic django tests..."
+#uv run python "$SCRIPT_DIR/../textbook_marketplace/manage.py" test
 
 echo "Running tests using pytest..."
 cd "$SCRIPT_DIR/../textbook_marketplace"
 uv run pytest
-sleep 60
+
+echo "Done, press Enter to exit..."
+read
