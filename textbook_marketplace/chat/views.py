7dbce4fab140670414048e8bd2115c311ee8462f
Sasha <Geimer0073@yandex.ru>
2025-05-08 22:02:08 +0500

    add view for retreiving messages tied to a user

--- textbook_marketplace/chat/views.py
+++ textbook_marketplace/chat/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
-# Create your views here.
+from django.contrib.auth import get_user_model
+from django.db.models import Q
+
+from .models import Message
+from .serializers import MessageSerializer
+
+User = get_user_model()
+
+
+class MessageView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        """ Returns list of messages request.user is member of. """
+        user = request.user
+        messages = Message.objects.filter(Q(sender=user) | Q(recipient=user))
+        serializer = MessageSerializer(messages, many=True)
+        return Response(serializer.data)
