faf9c2ed4ddfb3a8de8ee26b140321b2d71c4cbd
Sasha <Geimer0073@yandex.ru>
2025-05-08 21:51:19 +0500

    update ordering for messages and rm deletion on user del

--- textbook_marketplace/chat/models.py
+++ textbook_marketplace/chat/models.py
@@ -1,39 +1,25 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import AnonymousUser
 
 User = get_user_model()
 
 
-class Chat(models.Model):
-    name = models.CharField(max_length=255,
-                            blank=False,
-                            null=False)
-    members = models.ManyToManyField(User,
-                                     related_name='members')
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    def is_member(self, user: User) -> bool:
-        return user in self.members
-
-    def __str__(self):
-        return f'Chat: {self.name} - {self.members}'
-
-
 class Message(models.Model):
-    chat = models.ForeignKey(Chat,
-                             related_name='chat',
-                             on_delete=models.CASCADE)
+    """ Model for messages in user chats. """
     sender = models.ForeignKey(User,
                                related_name='message_sender',
-                               on_delete=models.CASCADE)
-    receiver = models.ForeignKey('marketplace.User',
-                                 related_name='message_receiver',
-                                 on_delete=models.CASCADE)
-    text = models.CharField(max_length=255, blank=True, null=False)
+                               on_delete=models.SET(AnonymousUser.id))
+    recipient = models.ForeignKey(User,
+                                  related_name='message_recipient',
+                                  on_delete=models.SET(AnonymousUser.id))
+    text = models.TextField()
     sent_at = models.DateTimeField(auto_now_add=True)
 
-    # class Meta:
-    #     ordering = # TODO
+    class Meta:
+        ordering = ['sender', 'recipient']
 
     def __str__(self):
-        return f'Sender_id: {self.sender.primary_key} - Text: {self.text[:10]}'
+        return (f'Sender: {self.sender.username}; '
+                f'Recipient: {self.recipient.username}; '
+                f'Text: {self.text[:15]}')
