0c2aca928697fcec5c361c5dce4a57b6fb6e7b53
Sasha <Geimer0073@yandex.ru>
2025-05-08 22:07:29 +0500

    add chat urls in project root

--- textbook_marketplace/textbook_marketplace/urls.py
+++ textbook_marketplace/textbook_marketplace/urls.py
@@ -6,5 +6,6 @@ from . import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('marketplace.urls')), 
+    path('api/', include('marketplace.urls')),
+    path('chat/', include('chat.urls'))
  ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
