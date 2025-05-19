2dfc821ed885b47d66542ac2d62c2fc315d4f566
Sasha <Geimer0073@yandex.ru>
2025-05-20 01:17:32 +0500

    add filtering and pagination default settings

--- textbook_marketplace/textbook_marketplace/settings.py
+++ textbook_marketplace/textbook_marketplace/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     # 'allauth.socialaccount',
     # 'allauth.socialaccount.providers.google', 
     # 'rest_framework.authtoken',
+    "django_filters",
     "rest_framework",
     "rest_framework_simplejwt",
     "corsheaders",
@@ -161,6 +162,11 @@ REST_FRAMEWORK = {
     # 'DEFAULT_PERMISSION_CLASSES': [
     #     'rest_framework.permissions.IsAuthenticated',
     # ],
+    'DEFAULT_PAGINATION_CLASS':
+        'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 20,
+    'DEFAULT_FILTER_BACKENDS':
+        ['django_filters.rest_framework.DjangoFilterBackend'],
 }
 
 CORS_ALLOWED_ORIGINS = [
