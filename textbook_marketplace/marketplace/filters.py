6c61948954e9002bc0203f44bc2ea0ab06fae880
Sasha <Geimer0073@yandex.ru>
2025-05-20 01:11:38 +0500

    create filter for textbooks

---
+++ textbook_marketplace/marketplace/filters.py
@@ -0,0 +1,32 @@
+import django_filters
+from django.db.models import Q
+
+from .models import Textbook
+
+
+class TextbookFilter(django_filters.FilterSet):
+    query = django_filters.CharFilter(method='in_title_or_desc',
+                                      label='Search')
+    author = django_filters.CharFilter(lookup_expr='icontains',
+                                       label='Author')
+    school_class = django_filters.CharFilter(label='Grade')
+    min_price = django_filters.NumberFilter(field_name='price',
+                                            lookup_expr='gte',
+                                            label='Minimal price')
+    max_price = django_filters.NumberFilter(field_name='price',
+                                            lookup_expr='lte',
+                                            label='Maximal price')
+    condition = django_filters.ChoiceFilter(choices=Textbook.CONDITION_CHOICES)
+    seller = django_filters.CharFilter(field_name='seller__username',
+                                       lookup_expr='icontains',
+                                       label='Seller')
+
+    class Meta:
+        model = Textbook
+        fields = ['query', 'author', 'school_class',
+                  'price', 'condition', 'seller']
+
+    def in_title_or_desc(self, queryset, name, value):
+        return queryset.filter(
+            Q(title__icontains=value) | Q(description__icontains=value)
+        )
